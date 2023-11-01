e3c44cdb69b56a1d5e59efd71586a70ae675b704
masomaster <masonlancaster@protonmail.com>
2023-10-31 21:47:08 -0700

    refines only show # of days in December

--- src/components/DayList/DayList.jsx
+++ src/components/DayList/DayList.jsx
@@ -9,9 +9,10 @@ export default function DayList({ setCurrentDay }) {
 
   // Render a span for each day in days.json up to the current day of the month in December
   const numberOfKeys = Object.keys(days).length;
-  const currentMonthDay = new Date().getDate();
+  // TODO: not entirely sure if this is right. do I need to add or subtract 1 from the maxDays?
+  const maxDays = new Date().getMonth() === 11 ? new Date().getDate() : 1000;
   const dayList = [];
-  for (let i = 0; i < currentMonthDay && i < numberOfKeys - 1; i++) {
+  for (let i = 0; i < maxDays && i < numberOfKeys - 1; i++) {
     dayList.push(
       <span
         className="dayListEntry"
