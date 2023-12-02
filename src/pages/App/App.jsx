874023c78acdbc473c506dbf9a6efea02042173f
masomaster <masonlancaster@protonmail.com>
2023-12-02 09:15:49 -0800

    defaults translation day to current date on initial load

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,40 +7,60 @@ import Home from "../Home/Home";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
-  const [currentDay, setCurrentDay] = useState(1);
-  const [maxDate, setMaxDate] = useState(1);
+  const [maxDate, setMaxDate] = useState(returnInitialMaxDate());
+  const [currentDay, setCurrentDay] = useState(maxDate);
 
-  // Toggle to manually set it to production mode
-  const isProduction = true;
-
-  useEffect(() => {
-    if (isProduction) {
-      calculateMaxDate();
-    } else {
-      setMaxDate(25);
-    }
-  });
-
-  // Determine how many days a user can see
-  function calculateMaxDate() {
+  function returnInitialMaxDate() {
     const currentDate = new Date().getDate();
     const currentYear = new Date().getFullYear();
     const isDecember = new Date().getMonth() === 11;
-    if (isProduction) {
-      // If during Dec '23, set maxDate to current date or 25, whichever is less
-      if (currentYear === 2023 && isDecember) {
-        currentDate < 26 ? setMaxDate(currentDate) : setMaxDate(25);
-      }
-      // If before Dec '23, allow only 1 day
-      if (currentYear === 2023 && !isDecember) {
-        setMaxDate(1);
-      }
-      // If after Dec '23, allow all 25 days
-      if (currentYear > 2023) {
-        setMaxDate(25);
-      }
+    // If during Dec '23, set maxDate to currentDate or 25, whichever is less
+    if (currentYear === 2023 && isDecember) {
+      return currentDate < 26 ? currentDate : 25;
     }
+    // If before Dec '23, allow only 1 day
+    if (currentYear === 2023 && !isDecember) {
+      return 1;
+    }
+    // If after Dec '23, allow all 25 days
+    if (currentYear > 2023) {
+      return 25;
+    } else return 25;
   }
+
+  // Toggle to manually set App to production mode
+  // const isProduction = true;
+  // OLD VERSION FOR TESTING
+  // If not in production mode, allow all days accessible for testing
+  // useEffect(() => {
+  //   if (isProduction) {
+  //     setMaxDate(returnInitialMaxDate());
+  //   } else {
+  //     setMaxDate(25);
+  //   }
+  // });
+
+  // OLD VERSION FOR TESTING
+  // Determine how many days a user can see
+  // function calculateMaxDate() {
+  //   const currentDate = new Date().getDate();
+  //   const currentYear = new Date().getFullYear();
+  //   const isDecember = new Date().getMonth() === 11;
+  //   if (isProduction) {
+  //     // If during Dec '23, set maxDate to currentDate or 25, whichever is less
+  //     if (currentYear === 2023 && isDecember) {
+  //       currentDate < 26 ? setMaxDate(currentDate) : setMaxDate(25);
+  //     }
+  //     // If before Dec '23, allow only 1 day
+  //     if (currentYear === 2023 && !isDecember) {
+  //       setMaxDate(1);
+  //     }
+  //     // If after Dec '23, allow all 25 days
+  //     if (currentYear > 2023) {
+  //       setMaxDate(25);
+  //     }
+  //   } else setMaxDate(25);
+  // }
 
   return (
     <main className="App">
